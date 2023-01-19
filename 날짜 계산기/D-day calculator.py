c8a2ffa05f61d8dcbf36f440ea8f50cc7503cc35
jh1004ben <88893644+wngud1004@users.noreply.github.com>
2023-01-20 00:01:25 +0900

    Update D-day calculator.py
    현재 날짜로부터 지정한 날짜와의 남은 날 수를 알려주는 계산기

--- 날짜 계산기/D-day calculator.py
+++ 날짜 계산기/D-day calculator.py
@@ -1,5 +1,32 @@
 from tkinter import *
-from mydays import *
+from datetime import *
+
+
+def get_today():
+    today = datetime.today()
+    return today.year, today.month, today.day
+
+
+def str_to_date(s):
+    try:
+        y = int(s[:4])
+        m = int(s[4:6])
+        d = int(s[6:])
+    except:
+        print("날짜 입력 오류")
+        return -1
+    else:
+        return datetime(y, m, d)
+
+
+def calc_days(date):
+    days = hours = 0
+    if str_to_date(date) != -1:
+        result = str_to_date(date) - datetime.today()
+        days = result.days
+        hours = result.seconds // 3600
+
+    return days, hours
 
 
 def f_click():
